63169fbd23b68ace9f7aea110955be4ecb6d1923
Avinash <avivhankalas45@gmail.com>
2022-10-17 16:58:02 +0530

    Working but finishing needed

--- app.py
+++ app.py
@@ -1,59 +1,141 @@
 import catboost
 
 import joblib
+import re
+from scipy import stats
 
 from flask import Flask,request,app,jsonify,url_for,render_template
 
 import numpy as np
 import pandas as pd
 
+# init app
 app = Flask(__name__)
 
+# model
 cat_model = joblib.load("catboost.joblib")
 
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 
 def home():
 
     return render_template('home.html')
 
+@app.route('/predict',methods=['GET','POST'])
 
-@app.route('/predict_api',methods=['POST'])
+def work():
+    str = request.form.get("Domain_Name")
 
+    op = []
+    op.append(len(str))                          # For length of string
+        
+    num = "".join(re.findall("[0-9]+",str))
+    if num.isnumeric():
+        op.append(1)
+    else:
+        op.append(0)                         # Domain names with only digits   
+            
+    vowels = 0
+    consonant = 0
+    specialChar = 0
+    digit = 0
+  
+    for i in range(0, len(str)): 
+          
+        ch = str[i] 
+  
+        if ( (ch >= 'a' and ch <= 'z') or 
+             (ch >= 'A' and ch <= 'Z') ): 
+  
+            ch = ch.lower()
+  
+            if (ch == 'a' or ch == 'e' or ch == 'i' 
+                        or ch == 'o' or ch == 'u'):
+                vowels += 1
+            else:
+                consonant += 1
+          
+        elif (ch >= '0' and ch <= '9'):
+            digit += 1
+        else:
+            specialChar += 1
+    
+    try:
+        ratio = (vowels / (vowels + consonant)) * 100
+        
+    except:
+        if((vowels + consonant) <= 0) :
+            ratio = 0
+    op.append(ratio)                         # To find vowel to consonent ratio 
+    
+    
+    if str.isalpha():
+        if len(str)==1:
+            op.append(ord(str))
+        else:
+            op.append(1)
+    else:
+        op.append(0)                        # Domain name with only one char
+            
+            
+    isNum = 0
+    for i in range(0, len(str)):
+        
+        ch = str[i]
+        
+        if (ch >= '0' and ch <= '9'):
+            isNum += 1
+        else:
+            pass
+    op.append(isNum)                       # Number of digits in domain name
+    
+    word = "".join(re.findall("[a-zA-Z]+",str))
+    word.lower()
+    cmp = "aeiou"
+    m_val = 0
+    t_tal = 0
+    for i in range(len(str)):
+        if str[i] in cmp:
+            t_tal += 1
+        elif str[i] not in cmp:
+            if m_val < t_tal:
+                m_val = t_tal
+                t_tal = 0
+        if i == len(str) - 1 :
+            if m_val < t_tal:
+                m_val = t_tal
+    op.append(m_val)                      # Max consicutive vowels 
+    
+    word = "".join(re.findall("[a-zA-Z]+",str))
+    word.lower()
+    cmp = "bcdfghjklmnpqrstvwxyz"
+    m_val = 0
+    t_tal = 0
+    for i in range(len(str)):
+        if str[i] in cmp:
+            t_tal += 1
+        elif str[i] not in cmp:
+            if m_val < t_tal:
+                m_val = t_tal
+                t_tal = 0
+        if i == len(str) - 1 :
+            if m_val < t_tal:
+                m_val = t_tal
+    op.append(m_val)
 
-def predict_api():
+    opp = stats.zscore(op)
 
-    data=request.json['data']
-    print(data)
+    prediction = cat_model.predict(opp)
 
-    print(np.array(list(data.values())).reshape(1,-1))
+    if prediction == 0:
+        
+        return render_template("home.html",prediction_text = "Domain Name Is Algorithmically Generated")
 
-    new_data=sc.transform(np.array(list(data.values())).reshape(1,-1))
+    else:
 
-    output= cat_model.predict(new_data)
-
-    print(output[0])
-
-    return jsonify(output[0])
-
-@app.route('/predict',methods=['POST'])
-
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input = sc.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    return render_template("home.html",pred_text=cat_model.predict(final_input)[0])
-
-@app.route('/pred',methods=['POST'])
-
-
-def pred():
-    data=[float(x) for x in request.form.values()]
-    final_input = sc.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    out = cat_model.predict(final_input)[0]
-    return render_template("home.html",pred_text=out)
+        
+        return render_template("home.html",prediction_text = "Domain Name Is LEGIT")
 
 if __name__=="__main__":
     app.run(debug=True)
